1977ebf883ec798352a6ee7f566e2a02821677a6
lasocorro <89995688+lasocorro@users.noreply.github.com>
2021-10-13 18:51:50 -0500

    comentarios hasta 42

--- carpeta/Conversion.sh
+++ carpeta/Conversion.sh
@@ -1,46 +1,46 @@
 #!/usr/bin/bash
-#titulo:cordenadas cartesianas a polares y viceversa
+#titulo: Coordenadas cartesianas a polares y viceversa
 #autor: Justin Santana, Laura Sofia Cortes, Juan Sebastian Sanchez
 #version:0.0.01
-pi=$(echo "scale=10; 4*a(1)" | bc -l) #se definen algunas variables
-echo "Bienvenido, este programa de conversion de coordenadas(polares y cartesianas)"
+pi=$(echo "scale=10; 4*a(1)" | bc -l) #se definen pi con 10 decimales
+echo "Bienvenido, programa de conversion de coordenadas (polares y cartesianas)"
 echo "Comencemos"
 s="1"
 while [ $s -eq "1" ] #se repetira el script hasta que s sea 0
-do
+do                   #hacer lo siguiente mientras s=1
 echo -e "Si desea pasar de coordenadas polares a cartesianas digite '1' si desea 
 lo contrario digite '2'"
 read v
 if [ $v == "1" ] #un condicional que hara algo diferente dependiendo de que numero elijamos
-then 
-echo "usted eligio 'coordenadas polares a cartesianas' "
-echo -e "Digite la magnitud del vector 'r'"
-read pr
+then             #si v=1 entonces hacer lo siguiente
+echo "Usted eligio 'coordenadas polares a cartesianas'"
+echo -e "Digite la magnitud del vector r"
+read pr                                                           #leer magnitud r
 echo "Ahora elija si su angulo esta en radianes o en grados"
-echo -e "1:radianes, 2:Grados"
-read pa
-    if [ $pa == "1" ]
-    then 
+echo -e "1:radianes, 2:Grados. Digite solamente el numero correspondiente"
+read pa                                                          #leer si el angulo esta en rad o en grados
+    if [ $pa == "1" ]                                            #si el angulo esta radianes
+    then                                                         #hacer lo siguiente
     echo "Usted elogio radianes"
-    echo -e "Ahora dijite el angulo"
-    read r
-    rg=$(echo "($r*180)/$pi" | bc -l)
-    echo "su conversion a grados es:$rg" #operaciones necesarias para hallar su equivalente
-    x=$(echo "$pr*c($r)" | bc -l)
-    y=$(echo "$pr*s($r)" | bc -l)
-    echo "la conversion de cordenadas polares a cartesianas es:x=$x y y=$y
+    echo -e "Ahora dijite el angulo en radianes"
+    read r                                                       #leer el angulo
+    rg=$(echo "($r*180)/$pi" | bc -l)                            #conversion de radianes a grados              
+    echo "Su angulo $r en grados es: $rg"                        #operaciones necesarias para hallar su equivalente van por debajo
+    x=$(echo "$pr*c($r)" | bc -l)                                #magnitud por coseno del angulo en rad
+    y=$(echo "$pr*s($r)" | bc -l)                                #magnitud por seno del angulo en rad
+    echo "La conversion de coordenadas polares a cartesianas es: x=$x y y=$y
     ($x,$y)"
-    echo "Desea hacer otra conversion?"
-    echo -e "1:si,2:no, digite el numero correspondiente"
-read last
-if [ $last == "1" ]
-then
-echo "Bienvenido"              #si lo desea puede terminar con el bucle
-elif [ $last == "2" ]
-then
+echo "¿Desea hacer otra conversion?"
+echo -e "1:si, 2:no, digite el numero correspondiente"
+read last                                                       #lea la desicion
+if [ $last == "1" ]                                             #si la respuesta es si
+then                                                            #diga lo siguiente
+echo "Bienvenido"                                               #si lo desea puede terminar con el bucle
+elif [ $last == "2" ]                                           #si la respuesta es no
+then                                                            #s=0 y se termina el bucle
 s="0"
-fi
-    elif [ $pa == "2" ]
+fi                                                     
+    elif [ $pa == "2" ]                                        
     then
     echo "Usted elogio grados"
     echo -e "Ahora dijite el angulo"
